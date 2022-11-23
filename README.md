44c0ee7fd9f3f8d460fbf9b3d6e2e43a8af39b2d
Kieran Hanrahan <kieran@knhn.me>
2022-11-23 11:36:52 -0500

    It work!

---
+++ README.md
@@ -0,0 +1,17 @@
+# Mute 6 Audio Tracks
+
+Plugin for Autodesk Flame software.
+
+Toggle audio tracks 1-6 between mute or not on the selected sequences/
+
+## Installation
+To make available to all users on the workstation, copy `mute_6_audio_tracks.py` to `/opt/Autodesk/shared/python`
+
+For specific users, copy to `/opt/Autodesk/user/<user name>/python`
+
+## Menus
+ - Right-click selected clips and/or sequences on the Desktop -> Mute... -> Toggle Audio Tracks 1-6
+ - Right-click selected clips and/or sequences in the Media Panel -> Mute... -> Toggle Audio Tracks 1-6
+
+## Acknowledgments
+UI Templates from [Logik-Portal/qt-snippets](https://github.com/logik-portal/qt_snippets)
---
+++ mute_6_audio_tracks.py
@@ -0,0 +1,17 @@
+# Go To Frame Number
+
+Plugin for Autodesk Flame software.
+
+Move the positioner to specified frame for selected clips and/or sequences.
+
+## Installation
+To make available to all users on the workstation, copy `go_to_frame_number.py` to `/opt/Autodesk/shared/python`
+
+For specific users, copy to `/opt/Autodesk/user/<user name>/python`
+
+## Menus
+ - Right-click selected clips and/or sequences on the Desktop -> Navigate... -> Go to Frame Number
+ - Right-click selected clips and/or sequences in the Media Panel -> Navigate... -> Go to Frame Number
+
+## Acknowledgments
+UI Templates from [Logik-Portal/qt-snippets](https://github.com/logik-portal/qt_snippets)
