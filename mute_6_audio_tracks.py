68da25dae536dd260a8791294178232f555e178f
Kieran Hanrahan <kieran@knhn.me>
2022-11-29 16:46:24 -0500

    Missing code replaced

--- mute_6_audio_tracks.py
+++ mute_6_audio_tracks.py
@@ -1,17 +1,82 @@
-# Go To Frame Number
+"""
+Mute 6 Audio Tracks
 
-Plugin for Autodesk Flame software.
+URL:
 
-Move the positioner to specified frame for selected clips and/or sequences.
+    http://github.com/khanrahan/mute-6-audio-tracks
 
-## Installation
-To make available to all users on the workstation, copy `go_to_frame_number.py` to `/opt/Autodesk/shared/python`
+Description:
 
-For specific users, copy to `/opt/Autodesk/user/<user name>/python`
+    Toggles audio tracks 1-6 between mute or not on the selected sequences.
 
-## Menus
- - Right-click selected clips and/or sequences on the Desktop -> Navigate... -> Go to Frame Number
- - Right-click selected clips and/or sequences in the Media Panel -> Navigate... -> Go to Frame Number
+Menus:
 
-## Acknowledgments
-UI Templates from [Logik-Portal/qt-snippets](https://github.com/logik-portal/qt_snippets)
+    Right-click selected clips and/or sequences on the Desktop Reels --> Mute...
+    --> Toggle Audio Tracks 1-6
+
+    Right-click selected clips and/or sequences in the Media Panel --> Mute...
+    --> Toggle Audio Tracks 1-6
+
+To Install:
+
+    For all users, copy this file to:
+    /opt/Autodesk/shared/python
+
+    For a specific user, copy this file to:
+    /opt/Autodesk/user/<user name>/python
+"""
+
+from __future__ import print_function
+
+__title__ = "Mute 6 Audio Tracks"
+__version_info__ = (0, 1, 1)
+__version__ = ".".join([str(num) for num in __version_info__])
+__title_version__ = "{} v{}".format(__title__, __version__)
+
+MESSAGE_PREFIX = "[PYTHON HOOK]"
+
+
+def message(string):
+    """Print message to shell window and append global MESSAGE_PREFIX."""
+
+    print(" ".join([MESSAGE_PREFIX, string]))
+
+
+def mute_6_tracks(selection):
+
+    message(__title_version__)
+    message("Script called from {}".format(__file__))
+
+    for sequence in selection:
+        for x in range(6):
+            try:
+                mute_status = sequence.audio_tracks[x].mute
+                if mute_status == False:
+                    sequence.audio_tracks[x].mute = True
+                    continue
+                if mute_status == True:
+                    sequence.audio_tracks[x].mute = False
+            except IndexError:
+                pass
+
+    message("Done!")
+
+
+def scope_clip(selection):
+
+    import flame
+
+    for item in selection:
+        if isinstance(item, flame.PySequence):
+            return True
+    return False
+
+
+def get_media_panel_custom_ui_actions():
+
+    return [{'name': "Mute...",
+             'actions': [{'name': "Toggle Audio Tracks 1-6",
+                          'isVisible': scope_clip,
+                          'execute': mute_6_tracks,
+                          'minimumVersion': "2020.3.1"}]
+            }]
